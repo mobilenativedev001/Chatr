c7ea643587f3b860b720162672217869d57499cd
Yoges <yogi2study@gmail.com>
2026-05-20 20:48:08 -0400

    Initial Commit

---
+++ Chatr/ChatrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatrApp.swift
+//  Chatr
+//
+//  Created by Yogeswaran Sivagurun - EXT on 2026-05-20.
+//
+
+import SwiftUI
+
+@main
+struct ChatrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
